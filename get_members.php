aca0857cd206d9b2af49a696e52e56ff95cd426b
pandiin122 <37384391+pandiin122@users.noreply.github.com>
2022-09-02 12:45:19 +0800

    Delete get_members.php

--- get_members.php
+++
@@ -1,8 +0,0 @@
-<?php
-$select_members = function ($id) {
-	include("connection.php");
-	$stmt = "SELECT COUNT(*) FROM martian WHERE base_id = $id";
-	$result = mysqli_query($conn, $stmt);
-
-	echo mysqli_fetch_array($result)[0];
-};
